87f3b49f09713cbfa1b5360b35465c729c3eaff2
bolaji0 <bjmustapha48@gmail.com>
2023-08-11 08:04:19 -0700

    Add project files.

---
+++ Arithmetic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NupatLinq_Class
+{
+    public class Arithmetic
+    {
+        public int Add(int num1  , int num2) 
+        {
+            return num1 + num2; ; 
+        }
+
+        public void TodaysDate()
+        {
+            Console.WriteLine($"Todays date is {DateTime.Now}");
+        }
+
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public int Subtraction(int num1, int num2) 
+        {
+            Console.WriteLine(Input(56, 38));
+            return num1 - num2;
+        }
+
+        private int Input(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace NupatLinq_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //  Arithmetic arithmetic = new Arithmetic();
+            // Console.WriteLine(arithmetic.Add(20,29));
+
+            // arithmetic.TodaysDate();
+            //Console.WriteLine(arithmetic.Subtraction(40, 30));
+            //Arithmetic.HelloWorld();
+            //Greetings("Dipo", "Deji");
+
+            //Linq 
+            //int[] arrayNum = { 267, 2720, 30, 3094, 23, 45, 34, 24, 22 };
+
+            //var orderAsc = from queryTo in arrayNum
+            //               orderby queryTo ascending
+            //               select queryTo;
+
+            //var orderDecs = from holdsDesc in arrayNum
+            //              orderby holdsDesc descending
+            //              select holdsDesc;
+
+
+            //foreach (var item in orderDecs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            string[] arrayStrings =
+            {
+                "Tomi Liys", "Sh ola", "Salako", "Shomolu",
+                "Abuja", "Lag os", "Nupat", "NIT"
+            };
+
+            var filter = from selectA in arrayStrings
+                         where selectA.Contains(" ")
+                         select selectA;
+
+            foreach (var array in filter)
+            {
+                Console.WriteLine(array);
+            }
+
+            
+
+
+
+            //Console.ReadLine(); 
+
+        }
+
+
+        static int Greetings(string firstName, string lastName)
+        {
+            Console.WriteLine($"Firstname {firstName}, lastname {lastName}");
+            return 0;
+        }
+
+       
+    }
+}
